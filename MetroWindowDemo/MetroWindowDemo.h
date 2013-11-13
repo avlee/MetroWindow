83a5cb5722dba5b1a79c77681a1b1f1434f542f0
XinGang Li <slinavlee@gmail.com>
2013-11-13 22:26:09 +0800

    Add MetroDialog test

--- MetroWindowDemo/MetroWindowDemo.h
+++ MetroWindowDemo/MetroWindowDemo.h
@@ -2,8 +2,26 @@
 
 #include "resource.h"
 #include "MetroWindow\MetroWindow.h"
+#include "MetroWindow\MetroDialog.h"
 
 using namespace MetroWindow;
+
+class CTestDialog : public CMetroDialog
+{
+public:
+    CTestDialog(HINSTANCE hInstance) : CMetroDialog(hInstance)
+    {
+    }
+
+    virtual ~CTestDialog(void)
+    {
+    }
+
+    virtual LPCTSTR GetWindowClassName() const
+    {
+        return _T("TestMetroDialog");
+    }
+};
 
 class CMainWindow : public CMetroWindow
 {
@@ -27,4 +45,34 @@ public:
         bHandled = TRUE;
         return 0;
     }
+
+    virtual LRESULT OnCreate(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+    {
+        LRESULT result = CMetroWindow::OnCreate(uMsg, wParam, lParam, bHandled);
+
+        btnTest_ = CreateWindow(L"BUTTON", L"显示模态对话框",
+                              WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                              100, 50, 150, 24,
+                              GetHWnd(), (HMENU)IDC_BTN_TEST, GetModuleInstance(), 0);
+
+        return result;
+    }
+
+    virtual LRESULT OnCommand(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+    {
+        int wmId = LOWORD(wParam);
+        if (wmId == IDC_BTN_TEST)
+        {
+            CTestDialog testWidnow(GetModuleInstance());
+            testWidnow.DoModal(IDD_DIALOG1, GetHWnd());
+            bHandled = TRUE;
+        }
+
+        return 0;
+    }
+
+private:
+    HWND btnTest_;
 };
+
+
